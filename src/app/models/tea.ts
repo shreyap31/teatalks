15c53ed2921e19ad4014146b2f94623dc7f92380
Naitik Shah <naitik.s@tcs.com>
2018-04-06 01:12:29 +0530

    Add TeaService for making tea related api calls

---
+++ src/app/models/tea.ts
@@ -0,0 +1,11 @@
+export class Tea {
+  id: string;
+  name: string;
+  teaBrand: string;
+  water: string;
+  milk: string;
+  sugar: string;
+  teaCupSize: string;
+  ingredients: string[];
+  createdBy: string;
+}
---
+++ src/app/services/tea.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ApiService } from './api.service';
+import { Tea } from '../models/tea';
+
+@Injectable()
+export class TeaService {
+
+  constructor(private apiService: ApiService) { }
+
+  getTeaList(userId = 'Default'): Observable<Tea[]> {
+    return this.apiService.request<Tea[]>(`/teaTalks/getTeaList/${userId}`);
+  }
+
+}
