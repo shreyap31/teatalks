9df0e9ef013bce28b1471c58b3ef1a493c665246
Naitik Shah <naitik.s@tcs.com>
2018-04-05 16:37:57 +0530

    Add base layout, order, and create components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OrderComponent } from './order/order.component';
+import { CreateComponent } from './create/create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: OrderComponent },
+  { path: 'order/:id', component: OrderComponent },
+  { path: 'create', component: CreateComponent },
+  { path: '**', redirectTo: '/' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { OrderComponent } from './order/order.component';
+import { CreateComponent } from './create/create.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    OrderComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
